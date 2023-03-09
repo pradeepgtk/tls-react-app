2252c1ea03dbafc69766ba5ac18a039478780589
Pradeep Kshirsagar <pradeep.k@gametion.com>
2023-03-09 10:25:33 +0530

    chnages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var raw = JSON.stringify({
+      name: "sdfsd",
+      pass: "ddsd"
+    });
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw
+    };
+    fetch("http://localhost:8000/data", requestOptions)
+      .then((response) => response.json())
+      .then((result) => setData(result))
+      .catch((error) => console.log("error", error));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{JSON.stringify(data)}</p>
     </div>
   );
 }
